24ca6b5e198ab99bce0448b7896201ceaf42a2c1
Michael Ferger <mferger92@gmail.com>
2016-06-21 10:06:05 -0600

    added about me section, self pic, and jquery links that scroll to about me section from header

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,30 @@
 (function() {
 	'use strict';
 
-$('.carousel').carousel();
-$('#myModal').modal()
-function toggleC(){
+	$('.carousel').carousel();
+	$('#myModal').modal()
+	function toggleC(){
 		$('.toggleCaption').hide()
 		var caption = carouselContainer.find('.active').find('.toggleCaption').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
-			function (){
+		function (){
 			$(this).removeClass('animated fadeInUp')
-	});
-	caption.slideToggle();
+		});
+		caption.slideToggle();
 	}
-$('#landingPage').on('click', function() {
-    $('#landingPage').slideUp('slow', 'swing')
+	$('#landingPage').on('click', function() {
+		$('#landingPage').slideUp({duration: 2000});
 		$('#body').removeClass('stop-scrolling')
-});
-
+	});
+	$('#aboutMe').click(function() {
+		$('html, body').animate({
+			scrollTop: $(document).height()
+		}, 2500);
+		return false;
+	});
+	$('#projects').click(function() {
+		$('html, body').animate({
+			scrollTop: '0px'
+		}, 2000);
+		return false;
+	});
 }());
